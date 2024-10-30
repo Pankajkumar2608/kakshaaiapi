c81efbb5145ef4e60236784b25cd8d4ed1de1f4c
Pankajkumar2608 <125138274+Pankajkumar2608@users.noreply.github.com>
2024-10-30 06:29:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,52 +1,33 @@
 const express = require('express');
-const { OpenAI }= require('openai'); 
+const { GoogleGenerativeAI } = require("@google/generative-ai") ;
 const dotenv = require('dotenv');
 dotenv.config();
 const app = express();
-
+const genAI = new GoogleGenerativeAI(process.env.API_KEY);
+const model = genAI.getGenerativeModel({
+  model: "gemini-1.5-flash",
+  systemInstruction: "You are Kaksha AI, a helpful assistant for JEE students, designed to help with Physics, Chemistry, and Math questions. Provide clear, concise explanations for their problems, using concepts relevant to JEE preparation and do not make assumption of answer. If asked, mention that your developer is Pankaj Jaat.",
+});
 
 const PORT = process.env.PORT ; 
 app.use(express.json());
 
-
-
-const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY 
-});
-
 app.post('/api/chat', async (req, res) => {
   try {
-    const response = await openai.chat.completions.create({
-      model: "gpt-4",
-      messages: [
-        {
-          role: "system",
-          content: "You are Kaksha AI, a helpful assistant for JEE students, designed to help with Physics, Chemistry, and Math questions. Provide clear, concise explanations for their problems, using concepts relevant to JEE preparation. If asked, mention that your developer is Pankaj Jaat."
-        },
-        {
-          role: "user",
-          content: req.body.prompt 
-        }
-      ],
-      max_tokens: 1000,
-      stream: true,
-    });
+    
+    const prompt = req.body.prompt;
+    const result = await model.generateContentStream(prompt);
 
-   
     res.setHeader('Content-Type', 'text/event-stream');
     res.setHeader('Cache-Control', 'no-cache');
     res.setHeader('Connection', 'keep-alive');
 
-    response.on('data', (chunk) => {
-      const data = JSON.parse(chunk.toString());
-      if (data.choices && data.choices[0].delta && data.choices[0].delta.content) {
-        res.write(data.choices[0].delta.content);
-      }
-    });
+    for await (const chunk of result.stream) {
+      const chunkText = chunk.text();
+      res.write(chunkText);
+    }
 
-    response.on('end', () => {
-      res.end();
-    });
+    res.end();
 
   } catch (error) {
     console.error("Error:", error);
@@ -57,3 +38,4 @@ app.post('/api/chat', async (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
 });
+
